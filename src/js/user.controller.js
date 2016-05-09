cc7592c20ba27e67afbbec618884e36905693ef8
Kazi Manzur Rashid <kaizmanzurrashid@gmail.com>
2016-05-09 20:39:29 +0600

    Added angular eslint plugin.

--- src/js/user.controller.js
+++ src/js/user.controller.js
@@ -1,11 +1,11 @@
 (function() {
 
   function UserController(github, flash) {
-    var self = this;
+    var vm = this;
     var userName = 'kazimanzurrashid';
 
     github.getUserProfile(userName).then(function(response) {
-      self.profile = response.data;
+      vm.profile = response.data;
       flash.success('User profile loaded successfully');
     }, function() {
       flash.error('Failed to load user profile');
